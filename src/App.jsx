5c26aa542592a2c9a825c015c4094fc31972a1f9
Rajesh97266 <rajesh97266@gmail.com>
2024-12-14 10:05:33 +0530

    use-state-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [settings, setSettings] = useState({
+    theme: "light",
+    fontSize: 16,
+  });
+
+  const toggleTheme = () => {
+    setSettings((prev) => ({
+      ...prev,
+      theme: prev.theme === "dark" ? "light" : "dark",
+    }));
+  };
+
+  const IncreaseFontSize = () => {
+    setSettings((prev) => ({
+      ...prev,
+      fontSize: prev.fontSize + 2,
+    }));
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Theme:{settings.theme}</h2>
+      <h3>Font Size:{settings.fontSize}px</h3>
+      <button onClick={toggleTheme}>Toggle Theme</button>
+      <button onClick={IncreaseFontSize}>Increase Font Size</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
